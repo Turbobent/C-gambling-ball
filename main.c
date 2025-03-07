ce172a66896a399339703736e9ed340089b3d881
Turbobent <kasp728h@edu.mercantec.dk>
2025-03-07 08:13:38 +0100

    added a menu

--- main.c
+++ main.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h> 
-#include <stdbool.h>
+#include <string.h>
 
-int generatematches() {
-    return rand() % 4; 
-}
-
-
-int withdraw(){}
+void addBalance(int *balance);
+void showLeaderboard();
 
 int main() { 
     char gameChoice[2];
@@ -17,81 +12,116 @@ int main() {
 
     srand(time(0)); 
     
- do{
-    int betChoice; 
-    int betamount;
-    
-    char* teams[] = {"randers fc", "agf", "bronby", "aalborg"};
-    
-    int team1Index = generatematches();
-    int team2Index = generatematches();
-
-    while (team1Index == team2Index) {
-        team2Index = generatematches();
-    }
-
-    char* playing[2] = {teams[team1Index], teams[team2Index]};
-
-    printf("Match: %s vs %s\n", playing[0], playing[1]);
-
     do {
-        printf("Enter 1 to bet on %s or 2 to bet on %s: ", playing[0], playing[1]);
-        scanf("%d", &betChoice);
-    } while (betChoice != 1 && betChoice != 2);
+        int betChoice; 
+        int betAmount;
+        
+        char* teams[] = {"Randers FC", "AGF", "Brondby", "Aalborg"};
+        
+        int team1Index = rand() % 4;
+        int team2Index = rand() % 4;
 
-    do {
-        printf("How much do you bet? ");
-        scanf("%d", &betamount);
-
-        if (betamount > balance) {
-            printf("Invalid bet! You only have %d.\n", balance);
+        while (team1Index == team2Index) {
+            team2Index = rand() % 4;
         }
-    } while (betamount > balance);
 
-    balance -= betamount; 
+        char* playing[2] = {teams[team1Index], teams[team2Index]};
 
-    int team1Score = rand() % 5;
-    int team2Score = rand() % 5;
+        printf("\nMatch: %s vs %s\n", playing[0], playing[1]);
 
-    printf("Final Score: %s %d - %d %s\n", playing[0], team1Score, team2Score, playing[1]);
+        do {
+            printf("Enter 1 to bet on %s or 2 to bet on %s: ", playing[0], playing[1]);
+            scanf("%d", &betChoice);
+        } while (betChoice != 1 && betChoice != 2);
 
-    if (team1Score > team2Score) {
-        printf("%s wins!\n", playing[0]);
+        do {
+            printf("How much do you bet? (Balance: %d) ", balance);
+            scanf("%d", &betAmount);
 
-        if (betChoice == 1) {
-            balance += betamount * 2; 
-            printf("You won the bet!\n");
+            if (betAmount > balance) {
+                printf("Invalid bet! You only have %d.\n", balance);
+            }
+        } while (betAmount > balance);
+
+        balance -= betAmount; 
+
+        int team1Score = rand() % 5;
+        int team2Score = rand() % 5;
+
+        printf("\nFinal Score: %s %d - %d %s\n", playing[0], team1Score, team2Score, playing[1]);
+
+        if (team1Score > team2Score) {
+            printf("%s wins!\n", playing[0]);
+            if (betChoice == 1) {
+                balance += betAmount * 2; 
+                printf("You won the bet!\n");
+            } else {
+                printf("You lost the bet!\n");
+            }
+        } else if (team1Score < team2Score) {
+            printf("%s wins!\n", playing[1]);
+            if (betChoice == 2) {
+                balance += betAmount * 2;
+                printf("You won the bet!\n");
+            } else {
+                printf("You lost the bet!\n");
+            }
         } else {
-            printf("You lost the bet!\n");
+            printf("It's a draw!\n");
+            balance += betAmount; 
         }
-    } else if (team1Score < team2Score) {
-        printf("%s wins!\n", playing[1]);
 
-        if (betChoice == 2) {
-            balance += betamount * 2;
-            printf("You won the bet!\n");
-        } else {
-            printf("You lost the bet!\n");
-        }
-    } else {
-        printf("It's a draw!\n");
-        balance += betamount; 
-    }
+        do {
+            printf("Play again? (Y/N/M for menu): ");
+            scanf("%s", gameChoice);
 
-    if(balance == 0){
-        do{
-            printf("balance is 0\n");
-            printf("add more money to make us rich min 5000$ ");
-            scanf("%d", &balance);
-        }while (balance < 5000); 
-    }
+            if (strcmp(gameChoice, "M") == 0 || strcmp(gameChoice, "m") == 0) {
+                int menuChoice;
+                do {
+                    printf("\n===== Menu =====\n");
+                    printf("1. Add Money\n");
+                    printf("2. View Leaderboard\n");
+                    printf("3. Play Game\n");
+                    printf("4. Quit\n");
+                    printf("Enter your choice: ");
+                    scanf("%d", &menuChoice);
 
-    printf("Your new balance is: %d\n", balance);
+                    switch (menuChoice) {
+                        case 1:
+                            addBalance(&balance);
+                            break;
+                        case 2:
+                            showLeaderboard();
+                            break;
+                        case 3:
+                            strcpy(gameChoice, "Y"); // Restart the game
+                            break;
+                        case 4:
+                            printf("Goodbye!\n");
+                            return 0;
+                        default:
+                            printf("Invalid choice! Try again.\n");
+                    }
+                } while (menuChoice != 3 && menuChoice != 4);
+            }
 
-    printf("play again Y/N ");
-    scanf("%s", gameChoice);
-    
+        } while (strcmp(gameChoice, "Y") != 0 && strcmp(gameChoice, "y") != 0 && 
+                 strcmp(gameChoice, "N") != 0 && strcmp(gameChoice, "n") != 0);
+
     } while (strcmp(gameChoice, "Y") == 0 || strcmp(gameChoice, "y") == 0);
 
     return 0;
+}
+
+void addBalance(int *balance) {
+    int amount;
+    printf("\nEnter amount to add: ");
+    scanf("%d", &amount);
+    *balance += amount;
+    printf("New balance: %d\n", *balance);
+}
+
+void showLeaderboard() {
+    printf("\n===== Leaderboard =====\n");
+    printf("Feature coming soon!\n");
 }
