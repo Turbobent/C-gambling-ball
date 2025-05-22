d353627a29ddcdc007b0a446e90e62f62e64acf7
Turbobent <kasp728h@edu.mercantec.dk>
2025-05-22 13:08:40 +0200

    random number color

--- numberAnimation.c
+++ numberAnimation.c
@@ -4,7 +4,7 @@
 #include <windows.h>
 
 void spinWheel(int min, int max) {
-    char colors[][8] = {"\033[0;31m", "\033[0;32m", "\033[0;33m", "\033[0;34m", "\033[0;35m", "\033[0;36m"};
+    char colors[][8] = {"\033[0;31m", "\033[0;32m", "\033[0;33m", "\033[0;34m", "\033[0;35m", "\033[0;36m"}; //ANSI Colors
     
     for (int i = 0; i < 3; i++) {
         int color_index = rand() % 6; // Pick random color
